cf5a05c84c0349bbb6e10a106a7cfa80cd1983ad
Ådne <aadnesd@stud.ntnu.no>
2019-02-15 14:41:03 +0100

    Added start to orders package

--- heis/orderHandling/orders/orders.go
+++ heis/orderHandling/orders/orders.go
@@ -2,10 +2,11 @@ package orders
 
 import(
 	"time"
-	"./../elevio"
-	"./../networkModule"
+	
 )
 
+//Not sure where it should be in the folder structure
+//Stud.ass: should create one transmitter with all required channels, not too big modules
 func SendOrders(sender chan <- elevio.ButtonEvent){
     orderTx := make(chan elevio.ButtonEvent)
     go bcast.Transmitter(16569, orderTx)  
