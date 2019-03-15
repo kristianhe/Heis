ad4b662d9c8177d844e3d460e74417a2b6a51eca
ludvigvl <ludvigvl@stud.ntnu.no>
2019-03-15 11:46:13 +0100

    working on stateMachine

--- stateMachine/internal_states.go
+++ stateMachine/internal_states.go
@@ -7,19 +7,20 @@ import (
 var master bool = false
 var mutex sync.Mutex
 
-// Functions
 func IsMaster() bool {
-
 	mutex.Lock()
 	defer mutex.Unlock()
-
 	return master
 }
 
-func SetMaster(local_master bool) {
+func IsConnected() bool {
+	mutex.Lock()
+	defer mutex.Unlock()
+ 
+}
 
+func SetMaster(local_master bool) {
 	mutex.Lock()
 	defer mutex.Unlock()
 	master = local_master
-
 }
